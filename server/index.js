64c5c38ca4d524a796ed80c7b58e066c9219401b
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2022-04-14 11:39:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://memories:memories123@cluster0.gksbv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://username:password@cluster0.gksbv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 
